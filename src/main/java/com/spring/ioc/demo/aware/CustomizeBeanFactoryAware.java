6c23115cc68c956ecac11f371c2049885f5631f1
eric he <ehe@accela.com>
2020-03-23 21:14:18 +0800

    对BeanFactoryAware实现用途描述

--- src/main/java/com/spring/ioc/demo/aware/CustomizeBeanFactoryAware.java
+++ src/main/java/com/spring/ioc/demo/aware/CustomizeBeanFactoryAware.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 
 /**
  * 在使用spring编程时，常常会遇到想根据bean的名称来获取相应的bean对象，这时候，就可以通过实现BeanFactoryAware来满足需求
+ * 实现 BeanFactoryAware 接口的 bean 可以直接访问 Spring 容器，被容器创建以后，它会拥有一个指向 Spring 容器的引用，
+ * 可以利用该bean根据传入参数动态获取被spring工厂加载的bean
  */
 @Component
 public class CustomizeBeanFactoryAware implements BeanFactoryAware {
